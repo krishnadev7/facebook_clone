f4626d12e26b4eb68cd2a22d9455b6cca816552f
krishnadev7 <krishnadevv12@gmail.com>
2021-08-29 17:57:41 +0530

    adding files

--- next.config.js
+++ next.config.js
@@ -1,3 +1,9 @@
 module.exports = {
-  reactStrictMode: true,
+  images:{
+    domains:[
+      'links.papareact.com',
+      'platform-looksaside.fbsbx.com',
+      'firebasestorage.googleapis.com',
+    ]
+  }
 }
